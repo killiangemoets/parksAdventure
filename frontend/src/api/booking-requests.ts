af997d1d5f064d9c6a8ae2ee24400a07f3ef9c36
Killian Gemoets <kilgem@live.fr>
2023-08-27 15:23:37 +0200

    cors

--- frontend/src/api/booking-requests.ts
+++ frontend/src/api/booking-requests.ts
@@ -66,20 +66,20 @@ export const validateOrder = async (token: string) => {
 
 export const getMyBookings = async () => {
   try {
-    // const response = await axiosInstance.get("/bookings/mine");
-    const fetchResponse = await fetch(
-      "http://localhost:3000/api/v1/bookings/mine",
-      {
-        mode: "cors",
-        credentials: "include",
-        headers: {
-          "Access-Control-Allow-Origin":
-            "https://national-parks-hiking-tours.vercel.app/",
-        },
-      }
-    );
+    const response = await axiosInstance.get("/bookings/mine");
+    // const fetchResponse = await fetch(
+    //   "http://localhost:3000/api/v1/bookings/mine",
+    //   {
+    //     mode: "cors",
+    //     credentials: "include",
+    //     headers: {
+    //       "Access-Control-Allow-Origin":
+    //         "https://national-parks-hiking-tours.vercel.app/",
+    //     },
+    //   }
+    // );
 
-    const response = await fetchResponse.json();
+    // const response = await fetchResponse.json();
 
     return response;
   } catch (err) {
