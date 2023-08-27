0d7c639e98ebad2031e3f299813e386fede3b6a9
Killian Gemoets <kilgem@live.fr>
2023-08-27 14:21:13 +0200

    cors

--- frontend/src/utils/axios/axios-instance.ts
+++ frontend/src/utils/axios/axios-instance.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   withCredentials: true,
-  // baseURL: process.env.REACT_APP_API_URL,
-  baseURL: "https://wild-teal-pelican-shoe.cyclic.cloud/api/v1",
+  baseURL: process.env.REACT_APP_API_URL,
+  // baseURL: "https://wild-teal-pelican-shoe.cyclic.cloud/api/v1",
 });
 
 export default axiosInstance;
