374a97ed6e9d2f6d0c45ebd66ac2b7373eae3ed3
Killian Gemoets <kilgem@live.fr>
2023-08-27 17:53:05 +0200

    env

--- frontend/src/utils/axios/axios-instance.ts
+++ frontend/src/utils/axios/axios-instance.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   withCredentials: true,
-  baseURL: process.env.REACT_APP_API_URL,
+  // baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://national-parks-hiking-tours-backend.onrender.com/api/v1/",
 });
 
 export default axiosInstance;
