c92d96242f294eeb4900546880d5e0c04642fd05
Killian Gemoets <kilgem@live.fr>
2023-09-29 14:15:14 +0200

    samSite

--- frontend/src/routes/userRoutes/userLogout/userLogout.style.tsx
+++ frontend/src/routes/userRoutes/userLogout/userLogout.style.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
 import { ValidationStepContainer } from "../../checkoutRoutes/validationStep/validationStep.style";
 
-export const UserLogoutContainer = styled(ValidationStepContainer)``;
+export const UserLogoutContainer = styled(ValidationStepContainer)`
+  min-height: calc(100vh - 24rem);
+`;
