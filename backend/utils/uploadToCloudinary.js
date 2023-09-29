feaa5435c20bceb79f769109d52950cbec567abb
Killian Gemoets <kilgem@live.fr>
2023-09-29 18:02:29 +0200

    cloudinary

--- backend/utils/uploadToCloudinary.js
+++ backend/utils/uploadToCloudinary.js
@@ -45,8 +45,10 @@ const uploadImage = async (image, folderPath = '') => {
     // return error;
     return {
       status: 'fail',
-      message:
-        'An unexpected problem occurred while uploading the pictures. Please try again.',
+      // message: error.message,
+      error,
+      // message:
+      //   'An unexpected problem occurred while uploading the pictures. Please try again.',
     };
   }
 };
