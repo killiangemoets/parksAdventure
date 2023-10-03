0f3c2bfb4a8bea6ee508c9d54bc27aa4871ad896
Killian Gemoets <kilgem@live.fr>
2023-10-03 10:37:21 +0200

    fix proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,9 +3,10 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   // Define the proxy middleware for your Express.js backend
   app.use(
-    "/api", // Change this to match the route you want to proxy
+    "/api/v1", // Change this to match the route you want to proxy
     createProxyMiddleware({
-      target: "https://wild-teal-pelican-shoe.cyclic.cloud/api/v1",
+      target: "https://wild-teal-pelican-shoe.cyclic.cloud/",
+      // target: "http://localhost:3000/",
       changeOrigin: true, // Enables cross-origin requests
     })
   );
--- frontend/src/utils/axios/axios-instance.ts
+++ frontend/src/utils/axios/axios-instance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   withCredentials: true,
   // baseURL: process.env.REACT_APP_API_URL,
-  baseURL: "/api",
+  baseURL: "/api/v1",
 });
 
 export default axiosInstance;
